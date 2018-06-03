f3ff531994af279fd60d65e567caf0f9fbbe5709
SenzaNome78 <36788151+SenzaNome78@users.noreply.github.com>
2018-06-03 22:55:35 +0200

    -

--- src/js/nuovoUtente.js
+++ src/js/nuovoUtente.js
@@ -46,7 +46,7 @@ $('#btnRegBadge').on('click', function () {
 				
 				if (badgeRegStatus == "S") // Registrazione badge avvenuta con successo
 				{
-					$('#newBadgeModalBody').addClass('stileModalSuccesso');
+					$('#newBadgeModalBody').addClass('_stileModalSuccesso');
 					$('#newBadgeModalBody').html(testoModalSuccessoBadge);
 					$('#newBadgeModalBody').removeClass('stileModalErrore');
 				}	
