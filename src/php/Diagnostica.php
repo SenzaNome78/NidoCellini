64ea0d1e6b1787ba7e554312b5591f347b9d99aa
SenzaNome78 <36788151+SenzaNome78@users.noreply.github.com>
2018-06-29 15:46:23 +0200

    -

--- src/php/Diagnostica.php
+++ src/php/Diagnostica.php
@@ -2,14 +2,12 @@
 
   // In questo script metto varie funzioni di diagnostica
   // Indica la funzione da eseguire
-  $paramCommand = isset($_GET['command']) ? $_GET['command'] : '';
+  $paramCommand = isset($_POST['command']) ? $_POST['command'] : '';
 
-  echo "TERS";
   // Passiamo il comando all'ESP8266
-  if ($paramCommand !== '')
+  if ($paramCommand !== "")
   {
       $ch = curl_init();
-
       curl_setopt($ch, CURLOPT_URL, "http://192.168.0.6/Diagnostica.html");
       curl_setopt($ch, CURLOPT_POST, 1);
       curl_setopt($ch, CURLOPT_POSTFIELDS, "command=$paramCommand");
